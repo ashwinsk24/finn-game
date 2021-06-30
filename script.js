57bf2ba3fb1a13f1e770029fc1167a609b7b4784
ashwinsk24 <71746986+ashwinsk24@users.noreply.github.com>
2021-06-30 21:53:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const fin = document.getElementById("fin");
+const objects = document.getElementById("objects");
+const score = document.getElementById("score");
+
+function jump() {
+  fin.classList.add("jump-animation");
+  setTimeout(() =>
+    fin.classList.remove("jump-animation"), 500);
+}
+
+document.addEventListener('keypress', (event) => {
+  if (!fin.classList.contains('jump-animation')) {
+    jump();
+  }
+})
+
+setInterval(() => {
+  const finTop = parseInt(window.getComputedStyle(fin)
+    .getPropertyValue('top'));
+  const objectsLeft = parseInt(window.getComputedStyle(objects)
+    .getPropertyValue('left'));
+  score.innerText++;
+
+  if (objectsLeft < 0) {
+    objects.style.display = 'none';
+  } else {
+    objects.style.display = ''
+  }
+
+  if (objectsLeft < 50 && objectsLeft > 0 && finTop > 150) {
+    alert("GAME OVER.\nSCORE : " + score.innerText +
+      "\nCLICK 'SPACE' OR 'OK' to Play again");
+    location.reload();
+  }
+}, 50);
+
